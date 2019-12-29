c4fdb2557def65fa38c77f1746d73f48ce1320e0
Felix Pojtinger <felix@pojtinger.com>
2019-12-29 03:59:43 +0100

    feat: Use new example as base for glue code, remove outdated edge implementation

--- pkg/supernode.c
+++ pkg/supernode.c
@@ -1,14 +1,16 @@
 #include "n2n/n2n.h"
 
+// See https://github.com/pojntfx/n2n/blob/dev/example_sn_embed.c
+
 int supernode_start(int listenPort, int managementPort)
 {
-    static n2n_sn_t sss_node;
-    static int keep_running;
+    int keep_running;
+    n2n_sn_t sss_node;
+    int rc;
 
     sn_init(&sss_node);
-
-    sss_node.lport = listenPort;
-    sss_node.daemon = 0;
+    sss_node.daemon = 0;   // Whether to daemonize
+    sss_node.lport = listenPort; // Main UDP listen port
 
     sss_node.sock = open_socket(sss_node.lport, 1);
     if (-1 == sss_node.sock)
@@ -16,12 +18,16 @@ int supernode_start(int listenPort, int managementPort)
         exit(-2);
     }
 
-    sss_node.mgmt_sock = open_socket(managementPort, 0);
+    sss_node.mgmt_sock = open_socket(managementPort, 0); // Main UDP management port
     if (-1 == sss_node.mgmt_sock)
     {
         exit(-2);
     }
 
     keep_running = 1;
-    return run_sn_loop(&sss_node, &keep_running);
+    rc = run_sn_loop(&sss_node, &keep_running);
+
+    sn_term(&sss_node);
+
+    return rc;
 }
