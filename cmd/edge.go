b55ec25b05addab4d3be05ab85493d118fd428df
Felix Pojtinger <felix@pojtinger.com>
2019-12-29 22:42:50 +0100

    feat: Make edge command 12-factor-app configuration compatible

--- cmd/edge.go
+++ cmd/edge.go
@@ -5,8 +5,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// EdgeCmd starts an edge.
-var EdgeCmd = &cobra.Command{
+var edgeCmd = &cobra.Command{
 	Use:   "edge",
 	Short: "Start an edge",
 	RunE: func(cmd *cobra.Command, args []string) error {
@@ -61,26 +60,26 @@ var (
 )
 
 func init() {
-	EdgeCmd.PersistentFlags().BoolVarP(&edgeAllowP2P, "allow-p2p", "p", true, "Whether to allow peer-to-peer connections. If false, all traffic will be routed through the supernode.")
-	EdgeCmd.PersistentFlags().BoolVarP(&edgeAllowRouting, "allow-routing", "r", true, "Whether to allow the node to route traffic to other nodes.")
-	EdgeCmd.PersistentFlags().StringVarP(&edgeCommunityName, "community-name", "c", "mynetwork", "The name of the n2n community to join.")
-	EdgeCmd.PersistentFlags().BoolVarP(&edgeDisablePMTUDiscovery, "disable-pmtu-discovery", "d", false, "Whether to allow peer-to-peer connections. If false, all traffic will be routed through the supernode.")
-	EdgeCmd.PersistentFlags().BoolVarP(&edgeDisableMulticast, "disable-multicast", "m", false, "Whether to disable multicast.")
-	EdgeCmd.PersistentFlags().BoolVarP(&edgeDynamicIPMode, "dynamic-ip-mode", "y", false, "Whether the IP address is set dynamically (see --address-mode). If the edge is running the network's DHCP server, this must be false.")
-	EdgeCmd.PersistentFlags().StringVarP(&edgeEncryptionKey, "encryption-key", "k", "mysecretkey", "The key to use for encryption.")
-	EdgeCmd.PersistentFlags().IntVarP(&edgeLocalPort, "local-port", "l", 0, "The local port to use. 0 uses any available port.")
-	EdgeCmd.PersistentFlags().IntVarP(&edgeManagementPort, "management-port", "a", 5644, "UDP management port. 5644 is the n2n default.")
-	EdgeCmd.PersistentFlags().IntVarP(&edgeRegisterInterval, "register-interval", "n", 1, "Interval in seconds for both UDP NAT hole punching and registration of the edge on the supernode.")
-	EdgeCmd.PersistentFlags().IntVarP(&edgeRegisterTTL, "register-ttl", "t", 1, "Interval in seconds for UDP NAT hole punching through the supernode.")
-	EdgeCmd.PersistentFlags().StringVarP(&edgeSupernodeHostPort, "supernode-host-port", "s", "localhost:1234", "Host:port of the supernode to connect to.")
-	EdgeCmd.PersistentFlags().IntVarP(&edgeTypeOfService, "type-of-service", "o", 16, "Type of service to use.")
-	EdgeCmd.PersistentFlags().IntVarP(&edgeEncryptionMethod, "encryption-method", "e", 2, "Method of encryption to use. 1 is no encryption, 2 is Twofish encryption, 3 is AES-CBC encryption. Twofish encryption is the n2n default, but only due to legacy compatibility reasons; AES-CBC is up to ten times faster.")
-	EdgeCmd.PersistentFlags().StringVarP(&edgeDeviceName, "device-name", "v", "edge0", "Name of the TUN/TAP device to create.")
-	EdgeCmd.PersistentFlags().StringVarP(&edgeAddressMode, "address-mode", "z", "static", "Mode of IP address assignment. \"static\" is a static assignment, \"dhcp\" uses the DHCP server at --device-ip (see --dynamic-ip-node). If the edge is running the network's DHCP server, this must be \"static\".")
-	EdgeCmd.PersistentFlags().StringVarP(&edgeDeviceIP, "device-ip", "i", "10.0.0.1", "IP address to set. Set to \"0.0.0.0\" if you are using \"dhcp\" as --address-mode. If the edge is running the network's DHCP server this must be set explicitly; i.e. to \"192.168.1.0\" if the DHCP server should give out addresses from \"192.168.1.10\" to \"192.168.1.100\".")
-	EdgeCmd.PersistentFlags().StringVarP(&edgeDeviceNetmask, "device-netmask", "q", "255.255.255.0", "The netmask to use.")
-	EdgeCmd.PersistentFlags().StringVarP(&edgeDeviceMACAddress, "device-mac-address", "x", "DE:AD:BE:EF:01:10", "The MAC address to use. Must be unique per edge.")
-	EdgeCmd.PersistentFlags().IntVarP(&edgeMTU, "mtu", "u", 1290, "The MTU to use.")
+	edgeCmd.PersistentFlags().BoolVarP(&edgeAllowP2P, "allow-p2p", "p", true, "Whether to allow peer-to-peer connections. If false, all traffic will be routed through the supernode.")
+	edgeCmd.PersistentFlags().BoolVarP(&edgeAllowRouting, "allow-routing", "r", true, "Whether to allow the node to route traffic to other nodes.")
+	edgeCmd.PersistentFlags().StringVarP(&edgeCommunityName, "community-name", "c", "mynetwork", "The name of the n2n community to join.")
+	edgeCmd.PersistentFlags().BoolVarP(&edgeDisablePMTUDiscovery, "disable-pmtu-discovery", "d", false, "Whether to allow peer-to-peer connections. If false, all traffic will be routed through the supernode.")
+	edgeCmd.PersistentFlags().BoolVarP(&edgeDisableMulticast, "disable-multicast", "m", false, "Whether to disable multicast.")
+	edgeCmd.PersistentFlags().BoolVarP(&edgeDynamicIPMode, "dynamic-ip-mode", "y", false, "Whether the IP address is set dynamically (see --address-mode). If the edge is running the network's DHCP server, this must be false.")
+	edgeCmd.PersistentFlags().StringVarP(&edgeEncryptionKey, "encryption-key", "k", "mysecretkey", "The key to use for encryption.")
+	edgeCmd.PersistentFlags().IntVarP(&edgeLocalPort, "local-port", "l", 0, "The local port to use. 0 uses any available port.")
+	edgeCmd.PersistentFlags().IntVarP(&edgeManagementPort, "management-port", "a", 5644, "UDP management port. 5644 is the n2n default.")
+	edgeCmd.PersistentFlags().IntVarP(&edgeRegisterInterval, "register-interval", "n", 1, "Interval in seconds for both UDP NAT hole punching and registration of the edge on the supernode.")
+	edgeCmd.PersistentFlags().IntVarP(&edgeRegisterTTL, "register-ttl", "t", 1, "Interval in seconds for UDP NAT hole punching through the supernode.")
+	edgeCmd.PersistentFlags().StringVarP(&edgeSupernodeHostPort, "supernode-host-port", "s", "localhost:1234", "Host:port of the supernode to connect to.")
+	edgeCmd.PersistentFlags().IntVarP(&edgeTypeOfService, "type-of-service", "o", 16, "Type of service to use.")
+	edgeCmd.PersistentFlags().IntVarP(&edgeEncryptionMethod, "encryption-method", "e", 2, "Method of encryption to use. 1 is no encryption, 2 is Twofish encryption, 3 is AES-CBC encryption. Twofish encryption is the n2n default, but only due to legacy compatibility reasons; AES-CBC is up to ten times faster.")
+	edgeCmd.PersistentFlags().StringVarP(&edgeDeviceName, "device-name", "v", "edge0", "Name of the TUN/TAP device to create.")
+	edgeCmd.PersistentFlags().StringVarP(&edgeAddressMode, "address-mode", "z", "static", "Mode of IP address assignment. \"static\" is a static assignment, \"dhcp\" uses the DHCP server at --device-ip (see --dynamic-ip-node). If the edge is running the network's DHCP server, this must be \"static\".")
+	edgeCmd.PersistentFlags().StringVarP(&edgeDeviceIP, "device-ip", "i", "10.0.0.1", "IP address to set. Set to \"0.0.0.0\" if you are using \"dhcp\" as --address-mode. If the edge is running the network's DHCP server this must be set explicitly; i.e. to \"192.168.1.0\" if the DHCP server should give out addresses from \"192.168.1.10\" to \"192.168.1.100\".")
+	edgeCmd.PersistentFlags().StringVarP(&edgeDeviceNetmask, "device-netmask", "q", "255.255.255.0", "The netmask to use.")
+	edgeCmd.PersistentFlags().StringVarP(&edgeDeviceMACAddress, "device-mac-address", "x", "DE:AD:BE:EF:01:10", "The MAC address to use. Must be unique per edge.")
+	edgeCmd.PersistentFlags().IntVarP(&edgeMTU, "mtu", "u", 1290, "The MTU to use.")
 
-	RootCmd.AddCommand(EdgeCmd)
+	rootCmd.AddCommand(edgeCmd)
 }
