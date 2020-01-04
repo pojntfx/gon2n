3f533033baa0e1a338bca45065a7edcedb61f38b
Felix Pojtinger <felix@pojtinger.com>
2020-01-04 22:26:27 +0100

    feat: Start adding supernode management server

--- pkg/svc/gon2n.go
+++ pkg/svc/gon2n.go
@@ -4,8 +4,12 @@ package svc
 
 import (
 	"context"
+
 	gon2n "github.com/pojntfx/gon2n/pkg/proto/generated/proto"
+	"github.com/pojntfx/gon2n/pkg/workers"
 	"gitlab.com/bloom42/libs/rz-go/v2/log"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // SupernodeManager manages supernodes.
@@ -15,7 +19,32 @@ type SupernodeManager struct {
 
 // Create creates a supernode.
 func (s *SupernodeManager) Create(_ context.Context, args *gon2n.SupernodeManagerCreateArgs) (*gon2n.SupernodeManagerCreateReply, error) {
-	log.Info("Creating supernode")
+	supernode := workers.Supernode{
+		ListenPort:     int(args.GetListenPort()),
+		ManagementPort: int(args.GetManagementPort()),
+	}
+
+	if err := supernode.Configure(); err != nil {
+		log.Error(err.Error())
+
+		return nil, status.Errorf(codes.Unknown, err.Error())
+	}
+
+	if err := supernode.OpenListenPortSocket(); err != nil {
+		log.Error(err.Error())
+
+		return nil, status.Errorf(codes.Unknown, err.Error())
+	}
+
+	if err := supernode.OpenManagementPortSocket(); err != nil {
+		log.Error(err.Error())
+
+		return nil, status.Errorf(codes.Unknown, err.Error())
+	}
+
+	go supernode.Start()
+
+	log.Info("Starting supernode")
 
 	return &gon2n.SupernodeManagerCreateReply{
 		Id: 1,
