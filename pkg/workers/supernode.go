f9249a2f7cbd14e59f5011673da5663acb6c83bd
Felicitas Pojtinger <felicitas@pojtinger.com>
2021-02-02 16:09:06 +0000

    feat: Port to n2n 2.8.0-stable

--- pkg/workers/supernode.go
+++ pkg/workers/supernode.go
@@ -1,6 +1,6 @@
 package workers
 
-//go:generate sh -c "rm -rf n2n; git clone https://github.com/pojntfx/n2n.git; cd n2n; ./autogen.sh; ./configure; make"
+//go:generate sh -c "rm -rf n2n; git clone https://github.com/ntop/n2n.git; cd n2n; git checkout 2.8-stable; ./autogen.sh; ./configure; make"
 
 /*
 #cgo CFLAGS: -g3 -Wall
